6bce71bb11fa619317d09d305e1d5b7cd6ca6ea3
Pushkar Anand <pushkar.anand@flipkart.com>
2020-05-20 02:09:08 +0530

    Base setup.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Abstracted`,
+    author: `Pushkar Anand`,
+    description: `Pushkar Anand's blog.`,
+    siteUrl: `https://blog.pushkaranand.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pushkar8723`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -34,8 +34,8 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in
+              India building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
@@ -50,7 +50,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed

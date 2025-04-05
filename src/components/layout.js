819fe2c50e57a415d0cea01eac5df93b00ed0d58
Pushkar Anand <pushkar8723@gmail.com>
2025-04-05 14:16:36 +0530

    feat: added twitter card and global styles to prevent content overflow

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,17 @@ import { Link, StaticQuery, graphql } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faCopyright, faRss } from '@fortawesome/free-solid-svg-icons';
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  img {
+    max-width: 100%;
+  }
+
+  iframe {
+    max-width: 100%;
+  }
+`;
 
 const HomeLink = styled.div`
   font-size: 24px;
@@ -98,45 +108,48 @@ const Layout = ({ location, title, children }) => {
         const { twitter, github, linkedin } = data.site.siteMetadata.social;
         const pages = data.pages.edges;
         return (
-          <div className="global-wrapper" data-is-root-path={isRootPath}>
-            <header className="global-header">
-              <HomeLink>{header}</HomeLink>
-              <Navbar>
-                <Links>
-                {
-                    pages.map(({ node }) => (
-                        <PageLink
-                            key={node.id}
-                            href={node.fields.slug}
-                        >
-                            {node.frontmatter.title}
-                        </PageLink>
-                    ))
-                }
-                </Links>
-            </Navbar>
-            </header>
-            <main>{children}</main>
-            <Footer>
-              <Social>
-                <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noreferrer noopener">
-                  <FontAwesomeIcon icon={faTwitter} style={{ width: '16px', height: '16px' }} />
-                </a>{` | `}
-                <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer noopener">
-                  <FontAwesomeIcon icon={faGithub} style={{ width: '16px', height: '16px' }} />
-                </a>{` | `}
-                <a href={`https://www.linkedin.com/${linkedin}`} target="_blank" rel="noreferrer noopener">
-                  <FontAwesomeIcon icon={faLinkedinIn} style={{ width: '16px', height: '16px' }} />
-                </a>{` | `}
-                <a href="/rss.xml">
-                  <FontAwesomeIcon icon={faRss} style={{ width: '16px', height: '16px' }} />
-                </a>
-              </Social>
-              <div>
-                <FontAwesomeIcon icon={faCopyright} style={{ width: '16px', height: '16px' }} /> {new Date().getFullYear()}
-              </div>
-            </Footer>
-          </div>
+          <>
+            <GlobalStyle />
+            <div className="global-wrapper" data-is-root-path={isRootPath}>
+              <header className="global-header">
+                <HomeLink>{header}</HomeLink>
+                <Navbar>
+                  <Links>
+                  {
+                      pages.map(({ node }) => (
+                          <PageLink
+                              key={node.id}
+                              href={node.fields.slug}
+                          >
+                              {node.frontmatter.title}
+                          </PageLink>
+                      ))
+                  }
+                  </Links>
+              </Navbar>
+              </header>
+              <main>{children}</main>
+              <Footer>
+                <Social>
+                  <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noreferrer noopener">
+                    <FontAwesomeIcon icon={faTwitter} style={{ width: '16px', height: '16px' }} />
+                  </a>{` | `}
+                  <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer noopener">
+                    <FontAwesomeIcon icon={faGithub} style={{ width: '16px', height: '16px' }} />
+                  </a>{` | `}
+                  <a href={`https://www.linkedin.com/${linkedin}`} target="_blank" rel="noreferrer noopener">
+                    <FontAwesomeIcon icon={faLinkedinIn} style={{ width: '16px', height: '16px' }} />
+                  </a>{` | `}
+                  <a href="/rss.xml">
+                    <FontAwesomeIcon icon={faRss} style={{ width: '16px', height: '16px' }} />
+                  </a>
+                </Social>
+                <div>
+                  <FontAwesomeIcon icon={faCopyright} style={{ width: '16px', height: '16px' }} /> {new Date().getFullYear()}
+                </div>
+              </Footer>
+            </div>
+          </>
         );
       }}
     />

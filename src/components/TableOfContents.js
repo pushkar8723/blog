dffe80bb324d61ebc6944487bc02e72bbd1c6f1e
Pushkar Anand <pushkar8723@gmail.com>
2025-04-10 18:23:49 +0000

    fix: changes as per review comments

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -75,7 +75,7 @@ export default function TableOfContents(props) {
                     }}
                 >
                     {items.map(item => (
-                        <li className={item.url === active && 'active'}>
+                        <li className={item.url === active ? 'active' : ''}>
                             <a href={item.url}>{item.title}</a>
                         </li>
                     ))}
